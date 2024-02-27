4a6e5a1713eb4090e8ef6f75f29e67e1a98e8efe
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-27 07:09:05 +0300

    Setup project

---
+++ BlackTracker/BlackTracker/Screens/MainScreenViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("There is a main view controller")
+        view.backgroundColor = .green
+    }
+
+
+}
+
