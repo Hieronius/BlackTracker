83636a8181c9406897a187ad905bb5b3f6a5fc76
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-27 08:00:46 +0300

    Download SnapKit library

--- BlackTracker/BlackTracker/Screens/View/MainScreenView.swift
+++ BlackTracker/BlackTracker/Screens/View/MainScreenView.swift
@@ -6,9 +6,16 @@ final class MainScreenView: UIView {
     
     init() {
         super.init(frame: .zero)
+        // setupProgressBar
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupProgressBar() {
+        
     }
 }
