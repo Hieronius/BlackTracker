5774ca5e3e1e60d3794c00ba3b9489814ead0bbc
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-27 08:54:41 +0300

    Add image view prototype

--- BlackTracker/BlackTracker/Screens/View/MainScreenView.swift
+++ BlackTracker/BlackTracker/Screens/View/MainScreenView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SnapKit
 
 final class MainScreenView: UIView {
     
@@ -6,7 +7,7 @@ final class MainScreenView: UIView {
     
     init() {
         super.init(frame: .zero)
-        // setupProgressBar
+        setupImageView()
     }
     
     required init(coder: NSCoder) {
@@ -15,7 +16,19 @@ final class MainScreenView: UIView {
     
     // MARK: - Private Methods
     
-    private func setupProgressBar() {
+    private func setupImageView() {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "tree")
         
+        self.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalToSuperview().offset(100)
+            make.bottom.equalToSuperview().offset(-100)
+        }
     }
 }
