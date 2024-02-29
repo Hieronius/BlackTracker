5aac331407e6254d6b73452c427ef829d2e4637c
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-29 05:01:03 +0300

    Add method to change images when user tapped it

--- BlackTracker/BlackTracker/Screens/MainScreenViewController.swift
+++ BlackTracker/BlackTracker/Screens/MainScreenViewController.swift
@@ -2,10 +2,15 @@ import UIKit
 
 final class MainScreenViewController: GenericViewController<MainScreenView> {
     
+    // MARK: - Private Properties
+    
+    private var currentProgressCount = 0
+    
     // MARK: - Initialization
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        rootView.delegate = self
     }
 
 
@@ -15,7 +20,14 @@ final class MainScreenViewController: GenericViewController<MainScreenView> {
 
 extension MainScreenViewController: MainScreenViewDelegate {
     func imageViewTapped() {
-        // method to get images from imageService and change the content of imageView
+        
+        if currentProgressCount == 9 {
+            currentProgressCount = 0
+            rootView.imageView.image = ImageService.images[currentProgressCount]
+        } else {
+            currentProgressCount += 1
+            rootView.imageView.image = ImageService.images[currentProgressCount]
+        }
     }
     
     
