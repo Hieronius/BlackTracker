3c51a6d8a997e0622c6a91b3b9f2fefa4cabc201
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-02 05:30:45 +0300

    Add HabitViewController

--- BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/HabitsListScreenViewController.swift
+++ BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/HabitsListScreenViewController.swift
@@ -1,1 +1,10 @@
 import UIKit
+
+final class HabitsListViewController: GenericViewController<HabitsListView> {
+    
+    // MARK: - Initialization
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
