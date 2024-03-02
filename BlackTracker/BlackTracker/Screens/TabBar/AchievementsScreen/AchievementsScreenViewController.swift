6c86fa3fc32158bcbd3ef56f5404d7f74eef5216
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-02 05:36:06 +0300

    Add achievement view and screen

--- BlackTracker/BlackTracker/Screens/TabBar/AchievementsScreen/AchievementsScreenViewController.swift
+++ BlackTracker/BlackTracker/Screens/TabBar/AchievementsScreen/AchievementsScreenViewController.swift
@@ -1,1 +1,10 @@
 import UIKit
+
+final class AchievementScreenViewController: GenericViewController<AchievementScreenView> {
+    
+    // MARK: - Initialization
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
