53496f5a8d9633752d4970a6dd187774b2e47c6c
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-02 05:29:01 +0300

    add HabitsListView

--- BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/View/HabitsListScreenView.swift
+++ BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/View/HabitsListScreenView.swift
@@ -1,1 +1,15 @@
 import UIKit
+
+final class HabitsListView: UIView {
+    
+    // MARK: - Initialization
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = AppColors.darkGray
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
