2f2fd693de6cd75ed774088dd51d9b6929d3d7f8
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-08 07:12:26 +0300

    Add CalendarCollectionCell

--- BlackTracker/BlackTracker/Screens/CalendarScreen/CalendarScreenViewController.swift
+++ BlackTracker/BlackTracker/Screens/CalendarScreen/CalendarScreenViewController.swift
@@ -5,6 +5,23 @@ final class CalendarScreenViewController: GenericViewController<CalendarScreenVi
     // MARK: - Private Properties
     
     // isDailyVisitsButtonTapped = false/true
+    private let selectedDate: Date
+//    private var baseDate: Date {
+//      didSet {
+//        days = generateDaysInMonth(for: baseDate)
+//        collectionView.reloadData()
+//        headerView.baseDate = baseDate
+//      }
+//    }
+
+    private let selectedDateChanged: ((Date) -> Void)
+
+    // should be placed on it's own method
+    private lazy var dateFormatter: DateFormatter = {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "d"
+      return dateFormatter
+    }()
     
     // MARK: - Initialization
     
@@ -18,6 +35,7 @@ final class CalendarScreenViewController: GenericViewController<CalendarScreenVi
 // MARK: - Day Generation
 private extension CalendarScreenViewController {
     
+    // Get actual data for month
     func monthMetadata(for baseDate: Date) throws -> MonthMetadata {
         guard let numberOfDaysInMonth = rootView.calendar.range(
             of: .day,
@@ -38,6 +56,82 @@ private extension CalendarScreenViewController {
             firstDayWeekday: firstDayWeekday
         )
     }
+    
+    func generateDaysInMonth(for baseDate: Date) -> [Day] {
+        guard let metadata = try? monthMetadata(for: baseDate) else {
+            fatalError("An error occurred when generating the metadata for \(baseDate)")
+        }
+        
+        let numberOfDaysInMonth = metadata.numberOfDays
+        let offsetInInitialRow = metadata.firstDayWeekday
+        let firstDayOfMonth = metadata.firstDay
+        
+        // code to avoid gaps in a month's first row
+        var days: [Day] = (1..<(numberOfDaysInMonth + offsetInInitialRow)).map { day in
+                let isWithinDisplayedMonth = day >= offsetInInitialRow
+                let dayOffset =
+                isWithinDisplayedMonth ?
+                day - offsetInInitialRow :
+                -(offsetInInitialRow - day)
+                
+                return generateDay(
+                    offsetBy: dayOffset,
+                    for: firstDayOfMonth,
+                    isWithinDisplayedMonth: isWithinDisplayedMonth
+                )
+            }
+        days += generateStartOfNextMonth(using: firstDayOfMonth)
+        
+        return days
+    }
+    
+    func generateDay(
+        offsetBy dayOffset: Int,
+        for baseDate: Date,
+        isWithinDisplayedMonth: Bool
+    ) -> Day {
+        let date = rootView.calendar.date(
+            byAdding: .day,
+            value: dayOffset,
+            to: baseDate)
+        ?? baseDate
+        
+        return Day(
+            date: date,
+            number: dateFormatter.string(from: date),
+            isSelected: rootView.calendar.isDate(date, inSameDayAs: selectedDate),
+            isWithinDisplayedMonth: isWithinDisplayedMonth
+        )
+    }
+    
+    func generateStartOfNextMonth(
+        using firstDayOfDisplayedMonth: Date
+    ) -> [Day] {
+        guard
+            let lastDayInMonth = rootView.calendar.date(
+                byAdding: DateComponents(month: 1, day: -1),
+                to: firstDayOfDisplayedMonth
+            )
+        else {
+            return []
+        }
+        
+        let additionalDays = 7 - rootView.calendar.component(.weekday, from: lastDayInMonth)
+        guard additionalDays > 0 else {
+            return []
+        }
+        
+        let days: [Day] = (1...additionalDays)
+            .map {
+                generateDay(
+                    offsetBy: $0,
+                    for: lastDayInMonth,
+                    isWithinDisplayedMonth: false)
+            }
+        
+        return days
+    }
+
     
     enum CalendarDataError: Error {
         case metadataGeneration
