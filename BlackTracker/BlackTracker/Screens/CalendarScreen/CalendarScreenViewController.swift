2cbe51e46d2f9e73cb29f61af4cd053ab394f288
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-08 06:24:33 +0300

    Remove unused code

--- BlackTracker/BlackTracker/Screens/CalendarScreen/CalendarScreenViewController.swift
+++ BlackTracker/BlackTracker/Screens/CalendarScreen/CalendarScreenViewController.swift
@@ -17,35 +17,31 @@ final class CalendarScreenViewController: GenericViewController<CalendarScreenVi
 
 // MARK: - Day Generation
 private extension CalendarScreenViewController {
-  // 1
-  func monthMetadata(for baseDate: Date) throws -> MonthMetadata {
-    // 2
-      guard let numberOfDaysInMonth = rootView.calendar.range(
-        of: .day,
-        in: .month,
-        for: baseDate)?.count,
-            let firstDayOfMonth = rootView.calendar.date(
+    
+    func monthMetadata(for baseDate: Date) throws -> MonthMetadata {
+        guard let numberOfDaysInMonth = rootView.calendar.range(
+            of: .day,
+            in: .month,
+            for: baseDate)?.count,
+              let firstDayOfMonth = rootView.calendar.date(
                 from: rootView.calendar.dateComponents([.year, .month], from: baseDate)
+              )
+        else {
+            throw CalendarDataError.metadataGeneration
+        }
+        
+        let firstDayWeekday = rootView.calendar.component(.weekday, from: firstDayOfMonth)
+        
+        return MonthMetadata(
+            numberOfDays: numberOfDaysInMonth,
+            firstDay: firstDayOfMonth,
+            firstDayWeekday: firstDayWeekday
         )
-      else {
-        // 3
-        throw CalendarDataError.metadataGeneration
     }
-
-    // 4
-      let firstDayWeekday = rootView.calendar.component(.weekday, from: firstDayOfMonth)
-
-    // 5
-    return MonthMetadata(
-      numberOfDays: numberOfDaysInMonth,
-      firstDay: firstDayOfMonth,
-      firstDayWeekday: firstDayWeekday
-    )
-  }
-
-  enum CalendarDataError: Error {
-    case metadataGeneration
-  }
+    
+    enum CalendarDataError: Error {
+        case metadataGeneration
+    }
 }
 
 // MARK: - CalendarScreenViewDelegate
