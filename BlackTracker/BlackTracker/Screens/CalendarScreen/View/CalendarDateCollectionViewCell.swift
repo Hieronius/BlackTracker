d960b727c28cbcc34f3b416fffe32caf83428e31
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-09 05:55:34 +0300

    Edit CalendarScreenViewCell file

--- BlackTracker/BlackTracker/Screens/CalendarScreen/View/CalendarDateCollectionViewCell.swift
+++ BlackTracker/BlackTracker/Screens/CalendarScreen/View/CalendarDateCollectionViewCell.swift
@@ -1,127 +1,135 @@
 import UIKit
 
 class CalendarDateCollectionViewCell: UICollectionViewCell {
-  private lazy var selectionBackgroundView: UIView = {
-    let view = UIView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    view.clipsToBounds = true
-    view.backgroundColor = .systemRed
-    return view
-  }()
-
-  private lazy var numberLabel: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    label.textAlignment = .center
-    label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
-    label.textColor = .label
-    return label
-  }()
-
-  private lazy var accessibilityDateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.calendar = Calendar(identifier: .gregorian)
-    dateFormatter.setLocalizedDateFormatFromTemplate("EEEE, MMMM d")
-    return dateFormatter
-  }()
-
-  static let reuseIdentifier = String(describing: CalendarDateCollectionViewCell.self)
-
-  var day: Day? {
-    didSet {
-      guard let day = day else { return }
-
-      numberLabel.text = day.number
-      accessibilityLabel = accessibilityDateFormatter.string(from: day.date)
-      updateSelectionStatus()
+    
+    // MARK: - Private Properties
+    
+    private var selectionBackgroundView: UIView!
+    private var numberLabel: UILabel!
+    
+    /// The date formatter used for accessibility.
+    private var accessibilityDateFormatter: DateFormatter!
+    
+    /// Reuse identifier for the cell.
+    static let reuseIdentifier = String(describing: CalendarDateCollectionViewCell.self)
+    
+    /// The date associated with this cell.
+    var day: Day? {
+        didSet {
+            guard let day = day else { return }
+            numberLabel.text = day.number
+            accessibilityLabel = accessibilityDateFormatter.string(from: day.date)
+            updateSelectionStatus()
+        }
     }
-  }
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-
-    isAccessibilityElement = true
-    accessibilityTraits = .button
-
-    contentView.addSubview(selectionBackgroundView)
-    contentView.addSubview(numberLabel)
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  override func layoutSubviews() {
-    super.layoutSubviews()
-
-    // This allows for rotations and trait collection
-    // changes (e.g. entering split view on iPad) to update constraints correctly.
-    // Removing old constraints allows for new ones to be created
-    // regardless of the values of the old ones
-    NSLayoutConstraint.deactivate(selectionBackgroundView.constraints)
-
-    // 1
-    let size = traitCollection.horizontalSizeClass == .compact ?
-      min(min(frame.width, frame.height) - 10, 60) : 45
-
-    // 2
-    NSLayoutConstraint.activate([
-      numberLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-      numberLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-
-      selectionBackgroundView.centerYAnchor.constraint(equalTo: numberLabel.centerYAnchor),
-      selectionBackgroundView.centerXAnchor.constraint(equalTo: numberLabel.centerXAnchor),
-      selectionBackgroundView.widthAnchor.constraint(equalToConstant: size),
-      selectionBackgroundView.heightAnchor.constraint(equalTo: selectionBackgroundView.widthAnchor)
-    ])
-
-    selectionBackgroundView.layer.cornerRadius = size / 2
-  }
-
-  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-    super.traitCollectionDidChange(previousTraitCollection)
-
-    layoutSubviews()
-  }
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        isAccessibilityElement = true
+        accessibilityTraits = .button
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    /// Sets up the subviews of the cell.
+    private func setupViews() {
+        setupBackground()
+        setupNumberLabel()
+        setupDateFormatter()
+    }
+    
+    private func setupBackground() {
+        selectionBackgroundView = UIView()
+        selectionBackgroundView.clipsToBounds = true
+        selectionBackgroundView.backgroundColor = .systemRed
+        
+        contentView.addSubview(selectionBackgroundView)
+        
+        selectionBackgroundView.snp.makeConstraints { make in
+            make.center.equalTo(numberLabel)
+            make.width.height.equalTo(45)
+        }
+        
+        selectionBackgroundView.layer.cornerRadius = 45 / 2
+    }
+    
+    private func setupNumberLabel() {
+        numberLabel = UILabel()
+        numberLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberLabel.textAlignment = .center
+        numberLabel.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        numberLabel.textColor = .label
+        
+        contentView.addSubview(numberLabel)
+        
+        numberLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    /// Sets up the date formatter for accessibility.
+    private func setupDateFormatter() {
+        accessibilityDateFormatter = DateFormatter()
+        accessibilityDateFormatter.calendar = Calendar(identifier: .gregorian)
+        accessibilityDateFormatter.setLocalizedDateFormatFromTemplate("EEEE, MMMM d")
+    }
+    
+    // MARK: - Layout
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    // MARK: - Helper Methods
+    
+    /// Updates the selection status of the cell.
+    private func updateSelectionStatus() {
+        guard let day = day else { return }
+        
+        if day.isSelected {
+            applySelectedStyle()
+        } else {
+            applyDefaultStyle(isWithinDisplayedMonth: day.isWithinDisplayedMonth)
+        }
+    }
+    
+    /// Checks if the screen size is small.
+    private var isSmallScreenSize: Bool {
+        let isCompact = traitCollection.horizontalSizeClass == .compact
+        let smallWidth = UIScreen.main.bounds.width <= 350
+        let widthGreaterThanHeight = UIScreen.main.bounds.width > UIScreen.main.bounds.height
+        
+        return isCompact && (smallWidth || widthGreaterThanHeight)
+    }
+    
+    /// Applies the selected style to the cell.
+    private func applySelectedStyle() {
+        accessibilityTraits.insert(.selected)
+        accessibilityHint = nil
+        
+        numberLabel.textColor = isSmallScreenSize ? .systemRed : .white
+        selectionBackgroundView.isHidden = isSmallScreenSize
+    }
+    
+    /// Applies the default style to the cell.
+    ///
+    /// - Parameter isWithinDisplayedMonth: A Boolean value indicating whether the date is within the displayed month.
+    private func applyDefaultStyle(isWithinDisplayedMonth: Bool) {
+        accessibilityTraits.remove(.selected)
+        accessibilityHint = "Tap to select"
+        
+        numberLabel.textColor = isWithinDisplayedMonth ? .label : .secondaryLabel
+        selectionBackgroundView.isHidden = true
+    }
 }
 
-// MARK: - Appearance
-private extension CalendarDateCollectionViewCell {
-  // 1
-  func updateSelectionStatus() {
-    guard let day = day else { return }
 
-    if day.isSelected {
-      applySelectedStyle()
-    } else {
-      applyDefaultStyle(isWithinDisplayedMonth: day.isWithinDisplayedMonth)
-    }
-  }
-
-  // 2
-  var isSmallScreenSize: Bool {
-    let isCompact = traitCollection.horizontalSizeClass == .compact
-    let smallWidth = UIScreen.main.bounds.width <= 350
-    let widthGreaterThanHeight = UIScreen.main.bounds.width > UIScreen.main.bounds.height
-
-    return isCompact && (smallWidth || widthGreaterThanHeight)
-  }
-
-  // 3
-  func applySelectedStyle() {
-    accessibilityTraits.insert(.selected)
-    accessibilityHint = nil
-
-    numberLabel.textColor = isSmallScreenSize ? .systemRed : .white
-    selectionBackgroundView.isHidden = isSmallScreenSize
-  }
-
-  // 4
-  func applyDefaultStyle(isWithinDisplayedMonth: Bool) {
-    accessibilityTraits.remove(.selected)
-    accessibilityHint = "Tap to select"
-
-    numberLabel.textColor = isWithinDisplayedMonth ? .label : .secondaryLabel
-    selectionBackgroundView.isHidden = true
-  }
-}
