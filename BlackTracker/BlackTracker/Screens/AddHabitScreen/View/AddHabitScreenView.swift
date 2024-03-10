a0f0af52521dbafd33221705081540966e433d9c
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-10 16:33:47 +0300

    Add settings and options stack view

--- BlackTracker/BlackTracker/Screens/AddHabitScreen/View/AddHabitScreenView.swift
+++ BlackTracker/BlackTracker/Screens/AddHabitScreen/View/AddHabitScreenView.swift
@@ -7,40 +7,51 @@ final class AddHabitScreenView: UIView {
     
     // MARK: - Public Properties
     
+    // view for all UI elements on the screen
+    var containerView: UIView!
+    
     // view for habit settings
     var settingsStackView: UIStackView!
     
     // habit name
+    var nameStackView: UIStackView!
     var nameLabel = "Name"
     var nameTextField: UITextField!
     
     // habit image
+    var imageStackView: UIStackView!
     var imageLabel = "Image"
     var imageView: UIImageView!
     
     // habit duration in minutes (may be tracking should be implemented later)
+    var durationStackView: UIStackView!
     var durationLabel = "Duration"
     var durationTextField: UITextField!
     
     // habit category
+    var categoryStackView: UIStackView!
     var categoryLabel = "Category"
     var categoryTextField: UITextField!
     
     // habit tag
+    var tagStackView: UIStackView!
     var tagLabel = "Tag"
     var tagTextField: UITextField!
     
     // habit frequency
+    var frequencyStackView: UIStackView!
     var frequencyLabel = "Frequency"
     var frequencyTextField: UITextField!
     
     // habit rate (daily, weekly, monthly)
+    var rateStackView: UIStackView!
     var rateLabel = "Rate"
     var rateTextField: UISegmentedControl!
     
     // view for skip/froze/edit/streak options
     var optionsStackView: UIStackView!
     
+    var streakStackView: UIStackView!
     var streakLabel = "Streak"
     var streakNumber = "0"
     
