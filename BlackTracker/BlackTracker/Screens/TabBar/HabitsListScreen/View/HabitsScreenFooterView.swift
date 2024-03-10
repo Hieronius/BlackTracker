1bc3ab05ff8aedfd4de7570876c64c31749e47b0
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-10 07:35:13 +0300

    Add HabitsCollectionCell initialization

--- BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/View/HabitsScreenFooterView.swift
+++ BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/View/HabitsScreenFooterView.swift
@@ -4,7 +4,42 @@ import SnapKit
 class HabitsScreenFooterView: UIView {
     
     // MARK: - Private Properties
+    private var stackView: UIStackView!
     
+    // MARK: - Initialization
     
-    // Init
+    init() {
+        super.init(frame: .zero)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 50
+        
+        let progressPoints = CustomRoundImageView()
+        progressPoints.configure(withNumber: 5, withTitle: "Grow Points")
+        stackView.addArrangedSubview(progressPoints)
+        
+        let addHabit = CustomRoundImageView()
+        addHabit.configure(withChar: "+", withBackgroundColor: AppColors.veryDarkGray)
+        stackView.addArrangedSubview(addHabit)
+        
+        self.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.leading.equalTo(safeAreaLayoutGuide).offset(-68)
+            make.trailing.equalTo(safeAreaLayoutGuide).offset(68)
+            make.bottom.equalTo(safeAreaLayoutGuide).offset(-30)
+            make.height.equalTo(50)
+        }
+    }
 }
