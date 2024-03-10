964252077a7f3f5ff1021041c779f08ddfdb1503
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-10 07:34:57 +0300

    Add HabitsHeaderView initialization

--- BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/View/HabitsScreenHeaderView.swift
+++ BlackTracker/BlackTracker/Screens/TabBar/HabitsListScreen/View/HabitsScreenHeaderView.swift
@@ -5,6 +5,23 @@ class HabitsScreenHeaderView: UIView {
     
     // MARK: - Private Properties
     
+    private var filterView: UISegmentedControl!
     
-    // Init
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupViews() {
+        filterView = UISegmentedControl()
+    }
 }
