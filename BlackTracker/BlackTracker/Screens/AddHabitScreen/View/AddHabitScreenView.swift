97ff8e4add772d2c1925d5c432262f2cfa356289
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-10 16:25:14 +0300

    Add settings and options of single habit

--- BlackTracker/BlackTracker/Screens/AddHabitScreen/View/AddHabitScreenView.swift
+++ BlackTracker/BlackTracker/Screens/AddHabitScreen/View/AddHabitScreenView.swift
@@ -3,13 +3,50 @@ import SnapKit
 
 final class AddHabitScreenView: UIView {
     
+    // should i use Navigation controller in my "add habit screen"???
+    
     // MARK: - Public Properties
     
     // view for habit settings
     var settingsStackView: UIStackView!
     
+    // habit name
+    var nameLabel = "Name"
+    var nameTextField: UITextField!
+    
+    // habit image
+    var imageLabel = "Image"
+    var imageView: UIImageView!
+    
+    // habit duration in minutes (may be tracking should be implemented later)
+    var durationLabel = "Duration"
+    var durationTextField: UITextField!
+    
+    // habit category
+    var categoryLabel = "Category"
+    var categoryTextField: UITextField!
+    
+    // habit tag
+    var tagLabel = "Tag"
+    var tagTextField: UITextField!
+    
+    // habit frequency
+    var frequencyLabel = "Frequency"
+    var frequencyTextField: UITextField!
+    
+    // habit rate (daily, weekly, monthly)
+    var rateLabel = "Rate"
+    var rateTextField: UISegmentedControl!
+    
     // view for skip/froze/edit/streak options
     var optionsStackView: UIStackView!
+    
+    var streakLabel = "Streak"
+    var streakNumber = "0"
+    
+    var editButton: UIButton!
+    var skipButton: UIButton!
+    var frozeButton: UIButton!
     
     // note field
     var noteFieldView: UITextField!
@@ -39,6 +76,7 @@ final class AddHabitScreenView: UIView {
     }
     
     private func setupSettings() {
+        settingsStackView = UIStackView()
         
     }
     
